6192cf12c6d93619c5c2bdca8ef22b16d8180622
David-Navarro-Oliver <navarropalle@gmail.com>
2026-04-29 10:01:11 +0200

    chore: initialize Spring Boot backend

---
+++ src/main/java/com/parque/BackendRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.parque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRepositoryApplication.class, args);
+	}
+
+}
