0c9b80f7a5a0f87c618ce716e217ac427c30c35b
Juan Luis Márquez <juan.luis.marquezcan@gmail.com>
2026-05-01 12:12:16 +0200

    feat: add users crud endpoints

---
+++ src/main/java/com/parque/user/service/UserService.java
@@ -0,0 +1,20 @@
+package com.parque.user.service;
+
+import com.parque.user.dto.UserCreateRequest;
+import com.parque.user.dto.UserResponse;
+import com.parque.user.dto.UserUpdateRequest;
+
+import java.util.List;
+
+public interface UserService {
+    List<UserResponse> getAll();
+
+    UserResponse getById(Long id);
+
+    UserResponse create(UserCreateRequest request);
+
+    UserResponse update(Long id, UserUpdateRequest request);
+
+    void delete(Long id);
+}
+
